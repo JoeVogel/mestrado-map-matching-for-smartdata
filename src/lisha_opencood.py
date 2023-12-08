85fa40b69aff6d133fe31f8be2e9e388982ce4b5
renatomeneghin <142346494+renatomeneghin@users.noreply.github.com>
2023-12-07 22:05:14 -0300

    Update lisha_opencood.py
    Adicionando leitura dos dados de IMU

--- src/lisha_opencood.py
+++ src/lisha_opencood.py
@@ -56,3 +56,45 @@ class LishaOpenCood():
 				points["latitude"].append(raw_data[i]['y'] * (180/np.pi))
   
 		return points
+
+        
+    	def get_IMU_iot_api(self):
+        
+	        IMU_Accel_x = []
+	        IMU_Accel_y = []
+	        IMU_Accel_z = []
+	        IMU_Rotation_x = []
+	        IMU_Rotation_y = []
+	        IMU_Rotation_z = []
+	        IMU_Gyro_x = []
+	        IMU_Gyro_y = []
+	        IMU_Gyro_z = []
+	        IMU_Compass = []
+	        
+	        raw_data_ax = self.make_request(0xC4962924,0)
+	        raw_data_ay = self.make_request(0xC4962924,1)
+	        raw_data_az = self.make_request(0xC4962924,2)
+	        raw_data_rx = self.make_request(0xC4B24924,0)
+	        raw_data_ry = self.make_request(0xC4B24924,1)
+	        raw_data_rz = self.make_request(0xC4B24924,2)
+	        raw_data_wx = self.make_request(0xC4B23924,0)
+	        raw_data_wy = self.make_request(0xC4B23924,1)
+	        raw_data_wz = self.make_request(0xC4B23924,2)
+	        raw_data_C = self.make_request(0xC4B24924,3)
+	        
+	        if (len(raw_data) > 0):
+	            for i in range(len(raw_data)-1):
+	                IMU_Accel_x = raw_data_ax[i]['value']
+	                IMU_Accel_y = raw_data_ay[i]['value']
+	                IMU_Accel_z = raw_data_az[i]['value']
+	                IMU_Rotation_x = raw_data_rx[i]['value']
+	                IMU_Rotation_y = raw_data_rx[i]['value']
+	                IMU_Rotation_z = raw_data_rx[i]['value']
+	                IMU_Gyro_x = raw_data_wx[i]['value']
+	                IMU_Gyro_y = raw_data_wx[i]['value']
+	                IMU_Gyro_z = raw_data_wx[i]['value']
+	                IMU_Compass = raw_data_C [i]['value']
+	        
+	        return IMU_Accel_x, IMU_Accel_y, IMU_Accel_z, IMU_Rotation_x , IMU_Rotation_y,IMU_Rotation_z ,IMU_Gyro_x,IMU_Gyro_y,IMU_Gyro_z,IMU_Compass
+	        
+	                
